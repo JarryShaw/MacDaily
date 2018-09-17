7510422f4c668e018eea4801ecd7322092d28e61
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-09-17 10:01:52 +0800

    Regular update
     - revised `release.sh`
     - new distribution

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as file:
 
 
 # version
-__version__ = '2018.09.16b1'
+__version__ = '2018.09.16'
 
 
 # set-up script for pip distribution
