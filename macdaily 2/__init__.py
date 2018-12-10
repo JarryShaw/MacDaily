05f680e3eb06d4982da98d8a5e8108a864ca9cc9
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-12-10 15:53:49 +0800

    Deduplicated

--- macdaily 2/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import platform
-
-from macdaily.util.error import UnsupportedOS
-
-# check platform
-if platform.system() != 'Darwin':
-    raise UnsupportedOS('macdaily: script runs only on macOS')
