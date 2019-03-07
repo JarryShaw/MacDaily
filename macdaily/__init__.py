b7d6f4de63cb2df0323a37fc6fbe3c616092dafa
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-03-07 18:29:03 +0800

    New distribution [2019.03.07.post1]
    * bugfix in
    * revised setup scripts
    * revised

--- macdaily/__init__.py
+++ macdaily/__init__.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import importlib
+import os
 import platform
+import warnings
 import sys
 
 import tbtrim
 
 from macdaily.util.const.macro import DEVMODE
-from macdaily.util.const.term import purple, red, reset
+from macdaily.util.const.term import red, reset
 from macdaily.util.error import Error, UnsupportedOS
 from macdaily.util.tools.misc import predicate
 
 # set up sys.excepthook
 if DEVMODE:
-    print('macdaily: {}info{}: devmode enabled'.format(purple, reset), file=sys.stderr)
+    warnings.showwarning('development mode enabled'.format(), RuntimeWarning,
+                         filename=__file__, lineno=0,
+                         line="MACDAILY_DEVMODE={}".format(os.environ['MACDAILY_DEVMODE']))
 else:
     tbtrim.set_trim_rule(predicate, strict=False,
                          target=(Error, KeyboardInterrupt))
