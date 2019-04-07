33a9a99fe7f232b2e83402c63ee0eefbfd5a38a6
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-04-07 14:35:45 +0800

    New distribution [2019.04.07.post1]
    * bugfix
    * no changes

--- macdaily/util/const/macro.py
+++ macdaily/util/const/macro.py
@@ -10,7 +10,7 @@ import sys
 from macdaily.util.compat import pathlib
 
 # version string
-VERSION = '2019.4.7'
+VERSION = '2019.4.7.post1'
 
 # terminal commands
 PYTHON = sys.executable         # Python version
